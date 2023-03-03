1623c10c40b8dc71e26a9c5599fcd86430676bfd
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2023-03-03 19:13:49 +0530

    ,

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-fetch("https://type.fit/api/quotes").then((res) => console.log(res));
+let p = fetch("https://type.fit/api/quotes");
+p.then((value) => {
+  return value.json();
+}).then((result) => {
+  let b = result;
+  //   let a = Math.random(1643);
+  b.forEach((element) => {
+    //1643 this many quates are in their
+    let a = Math.round(Math.random() * 1643);
+    // console.log(element);
+    // console.log(a);
+    // if(a===b[].){
+
+    // }
+  });
+  //   console.log(b);
+});
+let a = Math.random();
