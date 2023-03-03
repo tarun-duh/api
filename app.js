5c44911cfa258e5ba50e188a6c0e9c349a2bb5ca
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2023-03-03 23:13:20 +0530

    almost done

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-let p = fetch("https://type.fit/api/quotes");
-p.then((value) => {
-  return value.json();
-}).then((result) => {
-  let b = result;
-  //   let a = Math.random(1643);
-  b.forEach((element) => {
-    //1643 this many quates are in their
-    let a = Math.round(Math.random() * 1643);
-    // console.log(element);
-    // console.log(a);
-    // if(a===b[].){
-
-    // }
+function api() {
+  let p = fetch("https://type.fit/api/quotes");
+  p.then((value) => {
+    return value.json();
+  }).then((result) => {
+    let b = result;
+    let c = randomNum();
+    console.log(c);
+    for (let i in b) {
+      if (i == c) {
+        // console.log(b[i]);
+        update(b[i].author, b[i].text);
+        // function update() {
+        //   console.log(qoute);
+        //   qoute.innerHTML("");
+        // }
+      }
+    }
   });
-  //   console.log(b);
-});
+}
 let a = Math.random();
+function randomNum() {
+  let a = Math.round(Math.random() * 1643);
+  //   console.log("hey");
+  return a;
+}
+let b = randomNum();
+let button = document.querySelector("#button");
+let qoute = document.querySelector("#qoute");
+let name = document.querySelector("#name");
+function update(name1, qoute1) {
+  qoute.innerText = qoute1;
+  name.innerText = name1;
+}
+
+button.addEventListener("click", api);
